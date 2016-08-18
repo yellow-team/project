fc0f6dac4b5d04438444309e41a1e30c92af2b92
thequ_000 <thequ_000@Erin>
2016-08-18 20:55:06 +1000

    #17 Added new lines

--- eclipse-project/src/datamanagement/StudentUnitRecordProxy.java
+++ eclipse-project/src/datamanagement/StudentUnitRecordProxy.java
@@ -1,4 +1,5 @@
 package datamanagement;
+
 public class StudentUnitRecordProxy implements IStudentUnitRecord
 {
     private Integer studentId;
@@ -11,44 +12,53 @@ public class StudentUnitRecordProxy implements IStudentUnitRecord
         this.unitCode = unitCode;
         this.studentUnitRecordManger = StudentUnitRecordManager.getInstance();
     }
+
     public Integer getStudentId()
     {
         return studentId;
     }
+
     public String getUnitCode()
     {
         return unitCode;
     }
+
     public void setAsg1Mark(float asg1Mark)
     {
         studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         .setAsg1Mark(asg1Mark);
     }
+
     public float getAsg1Mark()
     {
         return studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         		.getAsg1Mark();
     }
+
     public void setAsg2Mark(float asg2Mark)
     {
         studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         .setAsg2Mark(asg2Mark);
     }
+
     public float getAsg2Mark()
     {
         return studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         		.getAsg2Mark();
     }
+
     public void setExamMark(float examMark)
     {
         studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         .setExamMark(examMark);
     }
+
     public float getExamMark()
     {
         return studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
         		.getExamMark();
     }
+
     public float calculateSumOfMarks()
     {
         return studentUnitRecordManger.getStudentUnitRecord(studentId, unitCode)
